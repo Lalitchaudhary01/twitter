05b925780213bf987a80d073c0ee6bede7ed0cf1
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-06-27 16:43:49 +0530

    setup

--- frontend/twitterclone/src/utils/constant.js
+++ frontend/twitterclone/src/utils/constant.js
@@ -1,5 +1,7 @@
-export const USER_API_END_POINT = "https://twitter-lalit.vercel.app/user";
-export const TWEET_API_END_POINT = "https://twitter-lalit.vercel.app/tweet";
+export const USER_API_END_POINT =
+  "https://twitter-lalit.vercel.app/api/v1/user";
+export const TWEET_API_END_POINT =
+  "https://twitter-lalit.vercel.app/api/v1/tweet";
 
 export const timeSince = (timestamp) => {
   let time = Date.parse(timestamp);
